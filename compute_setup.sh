33a70e6fa726ec9bd705e1cc358ac5ce7e5a8de2
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-09 16:40:27 +0200

    Update compute_setup.sh

--- compute_setup.sh
+++ compute_setup.sh
@@ -13,4 +13,33 @@ systemctl disable open-iscsi
 systemctl stop iscsid
 systemctl disable iscsid
 
+echo "root:gprm8350" | chpasswd
+
+chmod -x /etc/update-motd.d/*
+
+cat << EOF | tee /etc/update-motd.d/01-custom
+#!/bin/sh
+echo "****************************WARNING****************************************
+UNAUTHORISED ACCESS IS PROHIBITED. VIOLATORS WILL BE PROSECUTED.
+*********************************************************************************"
+EOF
+
+chmod +x /etc/update-motd.d/01-custom
+
+cat << EOF | tee /etc/modprobe.d/qemu-system-x86.conf
+options kvm_intel nested=1
+EOF
+
+modprobe -r kvm_intel
+
+modprobe kvm_intel nested=1
+
+cat /sys/module/kvm_intel/parameters/nested
+
+modinfo kvm_intel | grep -i nested
+
+mkdir -p /etc/apt/sources.list.d
+
+apt update -y
+
 reboot

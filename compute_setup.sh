ea205edf0a130b7783d46ea61f413b5ef2a2e825
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-18 16:14:18 +0200

    Update compute_setup.sh

--- compute_setup.sh
+++ compute_setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 DEBIAN_FRONTEND=noninteractive apt update
-DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-simplejson
+DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-simplejson mdadm
 DEBIAN_FRONTEND=noninteractive apt install -y qemu-kvm
 
 echo "configfs" >> /etc/modules

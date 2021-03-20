ce556d99b5ed666dd287e29844595dea132ea3ec
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-20 14:35:36 +0200

    Update deployment_setup.sh

--- deployment_setup.sh
+++ deployment_setup.sh
@@ -7,6 +7,6 @@ export LANG="en_US.UTF-8"
 DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt install -y python3-jinja2 python3-dev python3-venv python3-pip libffi-dev gcc libssl-dev curl git vim
 pip3 install -U pip
-cp etc.hosts /etc/hosts
+#cp etc.hosts /etc/hosts
 
-reboot
+#reboot

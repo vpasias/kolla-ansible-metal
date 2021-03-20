331a9a604ddc4b1f097f83ec851269c8c8fefe8b
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-20 14:56:14 +0200

    Update run-kolla.sh

--- run-kolla.sh
+++ run-kolla.sh
@@ -96,8 +96,8 @@ ssh -o StrictHostKeyChecking=no vagrant@server503 "lsblk && sudo vgs"
 # ---- PART TWO ----
 # Install Ansible and Kolla-Ansible
 
+sudo bash controller_setup.sh
 sudo bash deployment_setup.sh
-#sudo bash controller_setup.sh
 
 sudo apt update -y && sudo apt install python3-dev libffi-dev gcc libssl-dev python3-venv python3-pip -y
 
